823c0239f24e0e91f7791821bb345bc75a67dad5
Cheng Zhao <zcbenz@gmail.com>
2014-09-21 20:07:18 +0800

    Fix undefined char16.

--- src/base/strings/string_util_constants.cc
+++ src/base/strings/string_util_constants.cc
@@ -38,10 +38,6 @@ const wchar_t kWhitespaceWide[] = {
   WHITESPACE_UNICODE
 };
 
-const char16 kWhitespaceUTF16[] = {
-  WHITESPACE_UNICODE
-};
-
 const char kWhitespaceASCII[] = {
   0x09,    // CHARACTER TABULATION
   0x0A,    // LINE FEED (LF)
